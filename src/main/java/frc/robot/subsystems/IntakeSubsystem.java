9b1ab968aed42ac362e3f287a49c21f173853d48
Seth Mortenson <sethmortenson64@gmail.com>
2026-01-16 16:56:34 -0800

    Add formatter

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,16 +1,11 @@
-
 package frc.robot.subsystems;
 
-import com.ctre.phoenix6.controls.MotionMagicVoltage;
-import com.ctre.phoenix6.controls.PositionVoltage;
-import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.IntakeConstants;
 import frc.robot.commands.IntakeCommand;
-
 
 public class IntakeSubsystem extends SubsystemBase {
 
