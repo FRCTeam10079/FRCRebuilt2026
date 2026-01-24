ce6039df5296fdf9e50d0bcec1d90d8da52507eb
Risith Kankanamge <83322661+risithcha@users.noreply.github.com>
2026-01-24 15:25:28 -0800

    Add Telemetry class for robot state monitoring

--- src/main/java/frc/robot/Telemetry.java
+++ src/main/java/frc/robot/Telemetry.java
@@ -2,6 +2,7 @@ package frc.robot;
 
 import com.ctre.phoenix6.SignalLogger;
 import com.ctre.phoenix6.swerve.SwerveDrivetrain.SwerveDriveState;
+
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
@@ -20,6 +21,7 @@ import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj.util.Color8Bit;
 
 public class Telemetry {
+<<<<<<< HEAD
   private final double MaxSpeed;
 
   /**
