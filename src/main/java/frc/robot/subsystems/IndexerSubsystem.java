4a9181374f2c6bf106345356dcc2ac79baae8c00
Acer-15 <aaryangp12@gmail.com>
2026-01-29 20:02:30 -0800

    Added The Toggle Button For Intake

--- src/main/java/frc/robot/subsystems/IndexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/IndexerSubsystem.java
@@ -1,7 +1,6 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -13,10 +12,11 @@ public class IndexerSubsystem extends SubsystemBase {
   // private final TalonFX spindexerMotor; // Follower commented out to see if it helps with issues
 
   public IndexerSubsystem() {
-    String kCANbus = "canivore"; 
+    String kCANbus = "canivore";
 
     feederMotor = new TalonFX(IndexerConstants.kFeederMotorID, kCANbus);
-    // spindexerMotor = new TalonFX(IndexerConstants.kSpindexerMotorID, kCANbus); // Commented out to see if it helps with issues
+    // spindexerMotor = new TalonFX(IndexerConstants.kSpindexerMotorID, kCANbus); // Commented out
+    // to see if it helps with issues
 
     TalonFXConfiguration config = new TalonFXConfiguration();
 
@@ -41,12 +41,12 @@ public class IndexerSubsystem extends SubsystemBase {
 
     // Configure the FOLLOWER
     // We apply the same safety config to the follower too, just to be safe.
-    //Commented out to see if it helps with issues
+    // Commented out to see if it helps with issues
     // spindexerMotor.getConfigurator().apply(config);
 
     // Tell the follower to listen to the leader
     // Commented out to see if it helps with issues
-    //spindexerMotor.setControl(new Follower(IndexerConstants.kFeederMotorID, false));
+    // spindexerMotor.setControl(new Follower(IndexerConstants.kFeederMotorID, false));
   }
 
   public void setSpeed(double speed) {
