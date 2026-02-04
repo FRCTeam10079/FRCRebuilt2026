acee5d7bc2b669b55eae1693c8800f13307f6aaf
Risith Kankanamge <83322661+risithcha@users.noreply.github.com>
2026-02-03 17:32:46 -0800

    Fix/vision (#28)
    * Whoa vision last year was so weird | Upgrade how we use limelight vision and moved to 2026
    
    * Hopefully this fixes vision | Fixed timestamps and added some debugging
    
    * I am so stupid how did I forget about this | Connected telemetry to the actual thingy
    
    * Will CD help?
    
    * Yipeee! It works! | Added comments to the code explaining the fix

--- src/main/java/frc/robot/subsystems/IndexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/IndexerSubsystem.java
@@ -1,12 +1,11 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.hardware.TalonFX;
-import frc.robot.generated.TunerConstants;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.IndexerConstants;
+import frc.robot.generated.TunerConstants;
 
 public class IndexerSubsystem extends SubsystemBase {
 
@@ -14,9 +13,10 @@ public class IndexerSubsystem extends SubsystemBase {
   // private final TalonFX spindexerMotor; // Follower commented out to see if it helps with issues
 
   public IndexerSubsystem() {
-  // Use the generated CANBus instance instead of the deprecated String-based constructor
-  feederMotor = new TalonFX(IndexerConstants.kFeederMotorID, TunerConstants.kCANBus);
-    // spindexerMotor = new TalonFX(IndexerConstants.kSpindexerMotorID, kCANbus); // Commented out to see if it helps with issues
+    // Use the generated CANBus instance instead of the deprecated String-based constructor
+    feederMotor = new TalonFX(IndexerConstants.kFeederMotorID, TunerConstants.kCANBus);
+    // spindexerMotor = new TalonFX(IndexerConstants.kSpindexerMotorID, kCANbus); // Commented out
+    // to see if it helps with issues
 
     TalonFXConfiguration config = new TalonFXConfiguration();
 
@@ -41,12 +41,12 @@ public class IndexerSubsystem extends SubsystemBase {
 
     // Configure the FOLLOWER
     // We apply the same safety config to the follower too, just to be safe.
-    //Commented out to see if it helps with issues
+    // Commented out to see if it helps with issues
     // spindexerMotor.getConfigurator().apply(config);
 
     // Tell the follower to listen to the leader
     // Commented out to see if it helps with issues
-    //spindexerMotor.setControl(new Follower(IndexerConstants.kFeederMotorID, false));
+    // spindexerMotor.setControl(new Follower(IndexerConstants.kFeederMotorID, false));
   }
 
   public void setSpeed(double speed) {
