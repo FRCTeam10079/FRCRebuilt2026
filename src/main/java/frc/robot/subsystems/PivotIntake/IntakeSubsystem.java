1b4319c899157390871bdcee42bf6451214d69a3
Dhruv Hooda <Duv3tabest@gmail.com>
2026-02-12 00:32:51 +0000

    Conversion to RPM control for IntakeWheels

--- src/main/java/frc/robot/subsystems/PivotIntake/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/PivotIntake/IntakeSubsystem.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems.PivotIntake;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
@@ -12,8 +13,8 @@ import frc.robot.generated.TunerConstants;
 
 public class IntakeSubsystem extends SubsystemBase {
 
-  private final TalonFX intakeMotor =
-      new TalonFX(IntakeConstants.Wheels.MOTOR_ID, TunerConstants.kCANBus);
+  private final TalonFX intakeMotor = new TalonFX(IntakeConstants.Wheels.MOTOR_ID, TunerConstants.kCANBus);
+  private final VelocityVoltage m_velocityRequest = new VelocityVoltage(0).withSlot(0).withEnableFOC(true);
 
   public IntakeSubsystem() {
     configureIntakeMotor();
@@ -21,20 +22,32 @@ public class IntakeSubsystem extends SubsystemBase {
 
   private void configureIntakeMotor() {
     TalonFXConfiguration config = new TalonFXConfiguration();
-    config.MotorOutput.NeutralMode = NeutralModeValue.Coast; // wheels spin slowly to stop
+    config.MotorOutput.NeutralMode = NeutralModeValue.Coast;
     config.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+
     config.CurrentLimits.SupplyCurrentLimit = IntakeConstants.Wheels.SUPPLY_CURRENT_LIMIT;
     config.CurrentLimits.SupplyCurrentLimitEnable = true;
+    config.CurrentLimits.StatorCurrentLimit = IntakeConstants.Wheels.STATOR_CURRENT_LIMIT;
+    config.CurrentLimits.StatorCurrentLimitEnable = true;
+
+    config.Slot0 = config.Slot0
+        .withKA(IntakeConstants.Wheels.KA)
+        .withKV(IntakeConstants.Wheels.KV)
+        .withKD(IntakeConstants.Wheels.KD)
+        .withKS(IntakeConstants.Wheels.KS)
+        .withKI(IntakeConstants.Wheels.KI)
+        .withKP(IntakeConstants.Wheels.KP);
 
     intakeMotor.getConfigurator().apply(config);
   }
 
   public void intakeIn() {
-    intakeMotor.set(IntakeConstants.Wheels.SPEED);
+    // conversion of RPM to RPS
+    intakeMotor.setControl(m_velocityRequest.withVelocity(IntakeConstants.Wheels.INTAKE_RPM * 60.0));
   }
 
   public void stop() {
-    intakeMotor.stopMotor();
+    intakeMotor.setControl(m_velocityRequest.withVelocity(0));
   }
 
   // Intake Wheel Spin Command
